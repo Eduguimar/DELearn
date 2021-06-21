417e525c13eb04b4f526dbf1b880244af3708343
Eduardo Guimaraes <eduguimara@gmail.com>
2021-06-21 19:01:24 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/delearnbds/DelearnbdsApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.delearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelearnbdsApplication.class, args);
+	}
+}
