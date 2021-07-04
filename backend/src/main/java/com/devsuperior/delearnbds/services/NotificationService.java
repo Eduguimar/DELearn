592c9f60bb42742cb6d79f1d30a52a52ddf934f8
Eduardo Guimaraes <eduguimara@gmail.com>
2021-07-04 12:41:02 -0300

    Notifications for current user

---
+++ backend/src/main/java/com/devsuperior/delearnbds/services/NotificationService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.delearnbds.services;
+
+import com.devsuperior.delearnbds.dto.NotificationDTO;
+import com.devsuperior.delearnbds.entities.Notification;
+import com.devsuperior.delearnbds.entities.User;
+import com.devsuperior.delearnbds.repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private NotificationRepository repository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional(readOnly = true)
+    public Page<NotificationDTO> notificationsForCurrentUser(Pageable pageable) {
+        User user = authService.authenticated();
+        Page<Notification> pages = repository.findByUser(user, pageable);
+
+        return pages.map(notification -> new NotificationDTO(notification));
+    }
+
+}
